660d40dc127556a42a16974f0c619bc7677c3e7a
chenjunxyf <1040411807@qq.com>
2014-12-06 14:58:32 +0800

    modify style and config
    修改下配置文件和一些样式

--- ghost/config.js
+++ ghost/config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://localhost:2368',
+        url: 'http://www.chenjunxyf.me',
         mail: {},
         database: {
             client: 'sqlite3',
