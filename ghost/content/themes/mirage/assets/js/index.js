2128ca19ba6cd3eb09ab8328993048ebdf8652ae
chenjunxyf <1040411807@qq.com>
2015-05-10 17:33:27 +0800

    add directory of the post
    1. 给文章添加目录
    2. 取消平滑滚动

--- ghost/content/themes/mirage/assets/js/index.js
+++ ghost/content/themes/mirage/assets/js/index.js
@@ -13,6 +13,19 @@ $(function(){
       $('#menu a[href="'+ location.pathname +'"]').addClass("active");
     }
 
+    // blog article catalog
+    var dir = $('.directory');
+    if (dir.length > 0) {
+      var postDir = $('.post-content h2');
+      var catalog = $('.catalog');
+      var headers = [];
+      postDir.each(function(index) {
+        $(this).attr('id', 'h2-' + index);
+        headers.push('<li><a href="#h2-' + index + '">' + $(this).text() + '</a></li>');
+      });
+      catalog.append(headers.join(''));
+    }
+
     //scroll page on top
     $('.post-template .to_top a').click(function( event ) {
       event.preventDefault();
