80e67c37f1ca42df1769238fce71dc895e45c7bb
chenjunxyf <1040411807@qq.com>
2015-10-12 19:39:56 +0800

    update域名
    跟新域名

--- ghost/config.js
+++ ghost/config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://www.chenjunxyf.me',
+        url: 'http://www.chenjunxyf.info',
         mail: {},
         database: {
             client: 'sqlite3',
